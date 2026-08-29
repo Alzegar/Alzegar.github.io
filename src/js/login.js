1cb36cffea59d3badc159198938b04c4b4bc2d04
driped8 <pieroascarzacrisante10@gmail.com>
2026-08-29 10:31:50 -0500

    Correción de page principal

--- src/js/login.js
+++ src/js/login.js
@@ -71,7 +71,7 @@ formulario.addEventListener('submit', async (event) => {
     setTimeout(() => {
 
         window.location.href =
-            'src/pages/principal.html'
+            '../pages/principal.html'
 
     }, 300)
 
